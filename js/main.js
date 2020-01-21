e14a2cfa16556c52ae448092a01f454dc7a7b422
seongho7167 <54702206+seongho7167@users.noreply.github.com>
2020-01-21 11:59:31 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,4 +2,8 @@ $(document).ready(function(){
     $(".gnb_bc").click(function(){
         $(".ckBox").prop("checked", false);
     });
+    $(".about_container").on("mouseover", function(){
+        $(this).addClass("on");
+        $(this).siblings().removeClass("on");
+    });
 });
