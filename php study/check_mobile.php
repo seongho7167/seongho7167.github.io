2c6fc6c568a5bf5fc48e65afe5fe6c8b432936b6
seongho7167 <54702206+seongho7167@users.noreply.github.com>
2022-03-06 17:44:50 +0900

    Update check_mobile.php

--- php study/check_mobile.php
+++ php study/check_mobile.php
@@ -1,10 +1,9 @@
+
+<?php
 /*
 	Agent를 이용하여 사용자 접속환경이 모바일인지 pc인지 체크
 	pc 환경이라면 1(int), mobile 환경이라면 2(int)
 */
-
-<?php
-
 function check_mobile(){
 	$res = false;
 	$mobile_agent = "/(iPod|iPhone|Android|BlackBerry|SymbianOS|SCH-M\d+|Opera Mini|Windows CE|Nokia|SonyEricsson|webOS|PalmOS)/";
