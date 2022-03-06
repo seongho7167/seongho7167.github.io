9de05bf64d173bbf045e4d5b2032ce0b28a947c1
seongho7167 <54702206+seongho7167@users.noreply.github.com>
2022-03-06 16:21:41 +0900

    Add files via upload

---
+++ php study/client_ip.php
@@ -0,0 +1,23 @@
+/*
+	클라이언트 접속 ip 주소를 확인하고 리턴함
+*/
+<?php
+function client_ip() {
+	$res = '';
+	if (getenv('HTTP_CLIENT_IP'))
+		$res = getenv('HTTP_CLIENT_IP');
+	else if(getenv('HTTP_X_FORWARDED_FOR'))
+		$res = getenv('HTTP_X_FORWARDED_FOR');
+	else if(getenv('HTTP_X_FORWARDED'))
+		$res = getenv('HTTP_X_FORWARDED');
+	else if(getenv('HTTP_FORWARDED_FOR'))
+		$res = getenv('HTTP_FORWARDED_FOR');
+	else if(getenv('HTTP_FORWARDED'))
+		$res = getenv('HTTP_FORWARDED');
+	else if(getenv('REMOTE_ADDR'))
+		$res = getenv('REMOTE_ADDR');
+	else
+		$res = 'UNKNOWN';
+	return $res;
+}
+?>
