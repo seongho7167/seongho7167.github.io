a4ece5730a62ebebb4c8a432aae8af097ac496ca
seongho7167 <54702206+seongho7167@users.noreply.github.com>
2022-03-06 21:04:06 +0900

    Update escapestr.php

--- php study/escapestr.php
+++ php study/escapestr.php
@@ -3,9 +3,8 @@
 	sql 생성시 파라미터로 넘겨받은 값에 injection 공격이 들어오는것을 막기 위함 기존 mysqli_real_escape_string 절차형태에서 객체지향형태로 변형된 모습. mysqli 객체와 문자를 파라미터값으로 실행됨
 */
 function escapestr($str){
-	global $sysdb_conn;
 	$res = '';
-	$connection = new mysqli($sysdb_conn);
+	$connection = new mysqli($db_host,$db_user,$db_pw,$db);
 	$res = $connection->real_escape_string($str);
 	return $res;
 }
